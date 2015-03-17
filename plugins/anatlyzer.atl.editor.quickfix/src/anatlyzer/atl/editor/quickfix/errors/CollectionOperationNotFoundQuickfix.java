04ae5a31c4c8166a6a6d29a03e0b8e688b214ba2
juan <Juan.deLara@uam.es>
2015-03-17 14:31:32 +0100

    Improved quickfiz CollectionOperationNotFoundQuickfix

--- plugins/anatlyzer.atl.editor.quickfix/src/anatlyzer/atl/editor/quickfix/errors/CollectionOperationNotFoundQuickfix.java
+++ plugins/anatlyzer.atl.editor.quickfix/src/anatlyzer/atl/editor/quickfix/errors/CollectionOperationNotFoundQuickfix.java
@@ -19,6 +19,9 @@ import anatlyzer.atl.editor.quickfix.AbstractAtlQuickfix;
 import anatlyzer.atl.errors.atl_error.BindingWithoutRule;
 import anatlyzer.atl.errors.atl_error.CollectionOperationNotFound;
 import anatlyzer.atl.errors.atl_error.ModelElement;
+import anatlyzer.atl.types.CollectionType;
+import anatlyzer.atl.types.Metaclass;
+import anatlyzer.atl.types.impl.SequenceTypeImpl;
 import anatlyzer.atlext.ATL.Binding;
 import anatlyzer.atlext.ATL.Rule;
 import anatlyzer.atlext.OCL.CollectionOperationCallExp;
@@ -27,16 +30,19 @@ public class CollectionOperationNotFoundQuickfix extends AbstractAtlQuickfix {
 	private static final int threshold = 3;		// threshold distance to try an operation name with +1 or -1 params
 	// number of parameters X operation name
 	private static LinkedHashMap<Integer, List<String>> collectionOps = new LinkedHashMap<>();
-	private static HashMap<String, List<PrimTypes>> primitiveParam = new HashMap<>();
+	private static HashMap<String, List<CollType>> primitiveParam = new HashMap<>();
+	private String typeOfCollection;
 	
-	enum PrimTypes { 
+	enum CollType { 
 		Integer ("0"), 
-		String("''");
+		String("''"),
+		UserDefined("param");
 		
 		private String defaultLiteral;
 		
-		PrimTypes(String dl) { this.defaultLiteral = dl; }		
+		CollType(String dl) { this.defaultLiteral = dl; }		
 		public String defaultLiteral() { return this.defaultLiteral;}
+		public void setDefaultLiteral(String dl) { this.defaultLiteral = dl; }
 	}
 	
 	static {
@@ -46,9 +52,10 @@ public class CollectionOperationNotFoundQuickfix extends AbstractAtlQuickfix {
 	}
 	
 	static {
-		primitiveParam.put("at", Collections.singletonList(PrimTypes.Integer));
-		primitiveParam.put("subsequence", Arrays.asList(PrimTypes.Integer, PrimTypes.Integer));
-		primitiveParam.put("refGetValue", Collections.singletonList(PrimTypes.String));
+		primitiveParam.put("append", Collections.singletonList(CollType.UserDefined));
+		primitiveParam.put("at", Collections.singletonList(CollType.Integer));
+		primitiveParam.put("subsequence", Arrays.asList(CollType.Integer, CollType.Integer));
+		primitiveParam.put("refGetValue", Collections.singletonList(CollType.String));
 	}
 	
 	private int distance(String a, String b) { // Levenshtein distance
@@ -120,19 +127,19 @@ public class CollectionOperationNotFoundQuickfix extends AbstractAtlQuickfix {
 		int closestIndex = distances.get(numPar).indexOf(minDistance);
 		String closestOp = collectionOps.get(numPar).get(closestIndex);
 		System.out.println("Closest is "+closestOp);
-		return closestOp;			
-		
+		return closestOp;					
+	}
+	
+	private int getParamsClosest(String closest) {
+		for (int par : Arrays.asList(0, 1, 2)) {
+			if (collectionOps.get(par).contains(closest)) 
+				return par;			
+		}
+		return 0;
 	}
 
 	private void fixParams(int numP, String closest, IDocument document, int startPos, int endPos) {  // a bit redundant that we calculate this again...
-		int paramsClosest = 0;
-		
-		for (int par : Arrays.asList(0, 1, 2)) {
-			if (collectionOps.get(par).contains(closest)) {
-				paramsClosest = par;
-				break;
-			}
-		}
+		int paramsClosest = this.getParamsClosest(closest);
 		
 		if (paramsClosest > numP) {
 			System.out.println("You need to add "+(paramsClosest - numP )+" params");
@@ -164,16 +171,38 @@ public class CollectionOperationNotFoundQuickfix extends AbstractAtlQuickfix {
 		else System.out.println("number of params match.");
 	}
 	
+	private CollectionOperationCallExp getOperation() {
+		try {
+			CollectionOperationNotFound p = (CollectionOperationNotFound) getProblem();
+		
+			// p.getOperationName() is null at this point 
+			return (CollectionOperationCallExp)p.getElement();
+		} catch (CoreException e) {
+			
+		}
+		return null;
+	}
+	
 	@Override
 	public void apply(IDocument document) {
 
 		try {
-			CollectionOperationNotFound p = (CollectionOperationNotFound) getProblem();
 			
-			// p.getOperationName() is null at this point 
-			CollectionOperationCallExp elm = (CollectionOperationCallExp)p.getElement();
-			
+			CollectionOperationCallExp elm = this.getOperation();
+								
 			System.out.println("Collection operation "+elm.getOperationName()+"not found");
+			System.out.println("Type of collection "+elm.getSource().getInferredType());//+"\n of: "+elm.getSource().getInferredType());
+			this.typeOfCollection = "";
+			if (elm.getSource().getInferredType() instanceof CollectionType) {
+				CollectionType ct = (CollectionType)elm.getSource().getInferredType();				
+				if (ct.getContainedType() instanceof Metaclass) {
+					Metaclass mc = (Metaclass)ct.getContainedType();
+					System.out.println("Type of collection "+mc.getName());
+					this.typeOfCollection = mc.getName();
+					String metaModelName = mc.getModel().getName();
+					CollType.UserDefined.setDefaultLiteral(metaModelName+"!"+this.typeOfCollection+".newInstance()");
+				}
+			}
 			
 			String closest = this.getClosest(elm.getOperationName(), elm.getArguments().size());
 			
@@ -210,7 +239,15 @@ public class CollectionOperationNotFoundQuickfix extends AbstractAtlQuickfix {
 
 	@Override
 	public String getDisplayString() {
-		return "Choose a valid collection operation name";
+		CollectionOperationCallExp ce = this.getOperation();
+		String closest = this.getClosest(ce.getOperationName(), ce.getArguments().size());
+		int numP = ce.getArguments().size();
+		int closestPar = this.getParamsClosest(closest);
+		String params = "";
+		
+		if (numP < closestPar) params = " (and add "+(closestPar-numP)+"params)";
+		else if (numP > closestPar) params = " (and remove "+(numP-closestPar)+"params)";
+		return "Collection operation name "+ce.getOperationName()+" is invalid, change to "+closest+params;
 	}
 
 	@Override

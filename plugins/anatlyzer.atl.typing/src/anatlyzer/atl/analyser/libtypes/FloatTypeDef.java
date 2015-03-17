6415e0e57fafc6e79b1625f43d7313b999dd8512
jesus <jesus@jesus-PORTEGE-R830>
2015-03-17 23:17:32 +0100

    - Implemented Integer and Real built-in operations

--- plugins/anatlyzer.atl.typing/src/anatlyzer/atl/analyser/libtypes/FloatTypeDef.java
+++ plugins/anatlyzer.atl.typing/src/anatlyzer/atl/analyser/libtypes/FloatTypeDef.java
@@ -1,5 +1,6 @@
 package anatlyzer.atl.analyser.libtypes;
 
+import static anatlyzer.atl.analyser.libtypes.AtlTypes.*;
 import anatlyzer.atl.analyser.AnalyserContext;
 import anatlyzer.atl.types.Type;
 
@@ -12,8 +13,38 @@ public class FloatTypeDef extends AtlTypeDef {
 	}
 
 	public void init() {
-		// TODO Auto-generated method stub
+		// Ocl operations
+		// Operators hard-coded in the integer namepace
+		// comparison operators: <, >, >=, <=;
+		// binary operators: *, +, -, /, max(), min();
+		operation("max").
+			param("v", float_()).
+			returnType(float_());
+		operation("min").
+			param("v", float_()).
+			returnType(float_());
+		operation("abs").
+			returnType(float_());
 		
+		operation("floor").
+			returnType(integer());
+		operation("round").
+			returnType(integer());
+	
+		// Atl-specific operations
+		operation("longValue").
+			returnType(integer()); // should be "long", how is that possible in OCL?
+
+		operation("cos").returnType(float_());
+		operation("sin").returnType(float_());
+		operation("tan").returnType(float_());
+		operation("acos").returnType(float_());
+		operation("asin").returnType(float_());
+		operation("toDegrees").returnType(float_());
+		operation("toRadians").returnType(float_());
+		operation("exp").returnType(float_());
+		operation("log").returnType(float_());
+		operation("sqrt").returnType(float_());
 	}
 	
 }

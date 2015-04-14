580c7cbea55bab8dbd1417c1022ea4a09d833c63
juan <Juan.deLara@uam.es>
2015-04-14 10:28:59 +0200

    - MM2MM1_5.atl -> should give an error, but it does not - Started NoModelFoundQuickfix_ChooseExistingOne

---
+++ plugins/anatlyzer.atl.editor.quickfix/src/anatlyzer/atl/editor/quickfix/errors/NoModelFoundQuickfix_ChooseExistingOne.java
@@ -0,0 +1,45 @@
+package anatlyzer.atl.editor.quickfix.errors;
+
+import org.eclipse.core.resources.IMarker;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.jface.text.IDocument;
+
+import anatlyzer.atl.editor.quickfix.AbstractAtlQuickfix;
+import anatlyzer.atl.errors.atl_error.NoModelFound;
+import anatlyzer.atl.quickfixast.QuickfixApplication;
+
+
+public class NoModelFoundQuickfix_ChooseExistingOne extends AbstractAtlQuickfix {
+
+	public NoModelFound getProblem() {
+		try {
+			return (NoModelFound) super.getProblem();
+		} catch (CoreException ce) {
+			
+		}
+		return null;
+	}
+
+	@Override
+	public boolean isApplicable(IMarker marker) {		
+		return checkProblemType(marker, NoModelFound.class);
+	}
+
+	@Override
+	public void apply(IDocument document) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String getDisplayString() {
+		return "Model "+this.getProblem().getModelName()+" not found";
+	}
+
+	@Override
+	public QuickfixApplication getQuickfixApplication() throws CoreException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}

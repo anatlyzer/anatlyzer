76707dc308fbf8bdbfe535707262efa633dbf782
esther <esther.guerra@uam.es>
2015-04-22 13:00:20 +0200

    OperationCallInvalidParameterQuickfixSet

---
+++ plugins/anatlyzer.atl.editor.quickfix/src/anatlyzer/atl/editor/quickfix/kinds/OperationCallInvalidParameterQuickfixSet.java
@@ -0,0 +1,15 @@
+package anatlyzer.atl.editor.quickfix.kinds;
+
+import anatlyzer.atl.editor.quickfix.AbstractQuickfixSet;
+import anatlyzer.atl.editor.quickfix.AtlProblemQuickfix;
+import anatlyzer.atl.editor.quickfix.errors.OperationCallInvalidParameterQuickfix_AddCasting;
+
+public class OperationCallInvalidParameterQuickfixSet extends AbstractQuickfixSet  {
+		
+	@Override
+	public AtlProblemQuickfix[] getPossibleQuickfixes() {
+		return new AtlProblemQuickfix[] {
+				new OperationCallInvalidParameterQuickfix_AddCasting()
+		};
+	}
+}
